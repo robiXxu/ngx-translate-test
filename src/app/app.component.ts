f9dcaa079f25acd7a6aea1ea2087fa3046359e32
Schiriac Robert <schiriac.robert@gmail.com>
2019-01-11 15:28:14 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ngx-translate-test';
+  param = { value: 'world' };
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 }
